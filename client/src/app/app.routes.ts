46a1a9df7bc11a8fbc390a16e9d6d3191bed4c1a
pixou87 <tawatmouhamed@iclou.com>
2025-06-06 06:52:02 +0100

    feat: implémentation de la page FAQ avec affichage des questions

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -9,5 +9,9 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },  {
+    path: 'faq',
+    loadComponent: () => import('./faq/faq.page').then( m => m.FaqPage)
   },
+
 ];
