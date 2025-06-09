0a4812c897ef54b428228fa3c4c1c915434a8b71
russelinfo <yemelirussel@gmail.com>
2025-06-09 07:18:53 +0100

    application finale

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000' // L'URL de votre serveur backend
+  apiUrl: 'https://soutiens-moi-backend-mod.vercel.app' // L'URL de votre serveur backend
 };
